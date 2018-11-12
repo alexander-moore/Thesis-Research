449287f57d75f454d764e05243433ccc311946b1
Alexander Moore <31463017+moorea1@users.noreply.github.com>
2018-11-11 17:33:26 -0800

    Add files via upload

---
+++ Method Comparison.R
@@ -0,0 +1,310 @@
+library(keras)
+library(dplyr)
+library(survey)
+
+##################
+# We will test out naive, bootstrap, customLoss, and forest methods
+# with variable Y noise (epsilon)
+##################
+
+#
+## Creating a population to sample from 
+N <- 10^6
+n <- 10000
+ep_sig_control <- 2
+
+p_1 <- rnorm(N, mean = 30, sd = 2.5)
+p_2 <- rnorm(N, mean = 15, sd = 2)
+p_3 <- rnorm(N, mean = 5, sd = 1)
+
+epsilon <- rnorm(N, mean = 0, sd = ep_sig_control)
+p_y <- sqrt(p_1*p_2) + p_3 + epsilon
+
+
+epsilon_list <- c(0, 1, 2, 3)
+df_suitcase <- rep(NA, 4)
+
+rescale <- function(vec) { # rescale to 0,1
+  (vec - min(vec)) / (max(vec) - min(vec))
+}
+
+for (i in 1:4) {
+  ep <- epsilon_list[i]
+  pi_noise <- rnorm(N, mean = 0, sd = ep)
+  temp_pi <- sqrt(p_y) + pi_noise
+  
+  temp_pi <- rescale(temp_pi)
+  p_pi <- temp_pi * (n / sum(temp_pi))
+  
+  p_df <- cbind(p_1, p_2, p_3, p_pi, p_y)
+  p_tbl <- as_tibble(p_df)
+  
+  sample_population_by_pi <- sample_n(tbl = p_tbl, size = n, replace = FALSE, weight = p_pi)
+  
+  df_suitcase[[i]] <- sample_population_by_pi %>% rename(x_1 = p_1, #Since we are not dealing with p_ anymore
+                                                         x_2 = p_2,
+                                                         x_3 = p_3,
+                                                         pi = p_pi,
+                                                         y = p_y)
+}
+
+
+# Utility initialization
+create_split <- function(df) {
+  smp_size <- floor(.75*nrow(df))
+  train_ind <- sample(seq_len(nrow(df)), size = smp_size)
+  
+  x_train <<- df[train_ind, ]
+  x_test <<- df[-train_ind, ]
+  
+  y_train <<- y[train_ind]
+  y_test <<- y[-train_ind]
+}
+
+create_validation_split <- function(x_train, y_train) {
+  # Validation Set
+  val_indices <- 1:1000
+  
+  x_val <<- x_train[val_indices,]
+  partial_x_train <<- x_train[-val_indices,]
+  
+  y_val <<- y_train[val_indices]
+  partial_y_train <<- y_train[-val_indices]
+}
+
+normalize_data <- function(x_train, x_test) {
+  mean <- apply(x_train, 2, mean)
+  std <- apply(x_train, 2, sd)
+  x_train <<- scale(x_train, center = mean, scale = std)
+  x_test <<- scale(x_test, center = mean, scale = std)
+}
+
+##############################
+# Naive network, no pi
+# Traverse all of the datasets
+##############################
+
+loss_results_vec <- rep(NA, 4)
+mae_results_vec <- rep(NA, 4)
+
+for (i in 1:length(df_suitcase)) {
+  
+  df <- df_suitcase[[i]]
+  
+  y <- df$y
+  
+  df <- select(df, -c(pi, y))
+  
+  df <- as.matrix(df)
+  
+  # df processing
+  create_split(df)
+  create_validation_split(x_train, y_train)
+  normalize_data(x_train, x_test)
+  
+  # model definition
+  model <- keras_model_sequential() %>%
+    layer_dense(units = 6, activation = "relu", 
+                input_shape = dim(x_train)[[2]]) %>%
+    layer_dense(units = 6, activation = "relu") %>%
+    layer_dense(units = 1)
+  
+  model %>% compile(
+    optimizer = "rmsprop",
+    loss = "mse",
+    metrics = c("mae")
+  )
+  
+  history <- model %>% fit(
+    partial_x_train,
+    partial_y_train,
+    epochs = 40,
+    batch_size = 512,
+    validation_data = list(x_val, y_val)
+  )
+  
+  x_test <- as.matrix(x_test)
+  
+  BS_pi_results <- model %>% evaluate(x_test, y_test)
+  
+  loss_results_vec[i] <- BS_pi_results[1]
+  mae_results_vec[i] <- BS_pi_results[2]
+}
+
+loss_results_vec
+mae_results_vec
+
+##############################
+# Naive network, with pi
+# Traverse all of the datasets
+##############################
+
+loss_results_vec <- rep(NA, 4)
+mae_results_vec <- rep(NA, 4)
+
+for (i in 1:length(df_suitcase)) {
+  
+  df <- df_suitcase[[i]]
+  
+  y <- df$y
+  
+  df <- select(df, -c(y))
+  
+  df <- as.matrix(df)
+  
+  # df processing
+  create_split(df)
+  create_validation_split(x_train, y_train)
+  normalize_data(x_train, x_test)
+  
+  # model definition
+  model <- keras_model_sequential() %>%
+    layer_dense(units = 6, activation = "relu", 
+                input_shape = dim(x_train)[[2]]) %>%
+    layer_dense(units = 6, activation = "relu") %>%
+    layer_dense(units = 1)
+  
+  model %>% compile(
+    optimizer = "rmsprop",
+    loss = "mse",
+    metrics = c("mae")
+  )
+  
+  history <- model %>% fit(
+    partial_x_train,
+    partial_y_train,
+    epochs = 40,
+    batch_size = 512,
+    validation_data = list(x_val, y_val)
+  )
+  
+  x_test <- as.matrix(x_test)
+  
+  BS_pi_results <- model %>% evaluate(x_test, y_test)
+  
+  loss_results_vec[i] <- BS_pi_results[1]
+  mae_results_vec[i] <- BS_pi_results[2]
+}
+
+loss_results_vec
+mae_results_vec
+
+##############################
+# Bootstrap data Naive network, without pi
+# Traverse all of the datasets
+##############################
+loss_results_vec <- rep(NA, 4)
+mae_results_vec <- rep(NA, 4)
+
+for (i in 1:length(df_suitcase)) {
+  
+  df <- df_suitcase[[i]]
+  
+  weight_vec <- 1 / as.numeric(df$pi)
+  
+  df <- as_tibble(df)
+  
+  df <- sample_n(tbl = df, size = nrow(df), replace = TRUE, weight = weight_vec)
+  
+  y <- df$y
+  
+  df <- select(df, -c(pi, y))
+  
+  df <- as.matrix(df)
+  
+  # df processing
+  create_split(df)
+  create_validation_split(x_train, y_train)
+  normalize_data(x_train, x_test)
+  
+  # model definition
+  model <- keras_model_sequential() %>%
+    layer_dense(units = 6, activation = "relu", 
+                input_shape = dim(x_train)[[2]]) %>%
+    layer_dense(units = 6, activation = "relu") %>%
+    layer_dense(units = 1)
+  
+  model %>% compile(
+    optimizer = "rmsprop",
+    loss = "mse",
+    metrics = c("mae")
+  )
+  
+  history <- model %>% fit(
+    partial_x_train,
+    partial_y_train,
+    epochs = 40,
+    batch_size = 512,
+    validation_data = list(x_val, y_val)
+  )
+  
+  x_test <- as.matrix(x_test)
+  
+  BS_pi_results <- model %>% evaluate(x_test, y_test)
+  
+  loss_results_vec[i] <- BS_pi_results[1]
+  mae_results_vec[i] <- BS_pi_results[2]
+}
+
+loss_results_vec
+mae_results_vec
+
+##############################
+# Bootstrap data Naive network, with pi
+# Traverse all of the datasets
+##############################
+loss_results_vec <- rep(NA, 4)
+mae_results_vec <- rep(NA, 4)
+
+for (i in 1:length(df_suitcase)) {
+  
+  df <- df_suitcase[[i]]
+  
+  weight_vec <- 1 / as.numeric(df$pi)
+  
+  df <- as_tibble(df)
+  
+  df <- sample_n(tbl = df, size = nrow(df), replace = TRUE, weight = weight_vec)
+  
+  y <- df$y
+  
+  df <- select(df, -c(y))
+  
+  df <- as.matrix(df)
+  
+  # df processing
+  create_split(df)
+  create_validation_split(x_train, y_train)
+  normalize_data(x_train, x_test)
+  
+  # model definition
+  model <- keras_model_sequential() %>%
+    layer_dense(units = 6, activation = "relu", 
+                input_shape = dim(x_train)[[2]]) %>%
+    layer_dense(units = 6, activation = "relu") %>%
+    layer_dense(units = 1)
+  
+  model %>% compile(
+    optimizer = "rmsprop",
+    loss = "mse",
+    metrics = c("mae")
+  )
+  
+  history <- model %>% fit(
+    partial_x_train,
+    partial_y_train,
+    epochs = 40,
+    batch_size = 512,
+    validation_data = list(x_val, y_val)
+  )
+  
+  x_test <- as.matrix(x_test)
+  
+  BS_pi_results <- model %>% evaluate(x_test, y_test)
+  
+  loss_results_vec[i] <- BS_pi_results[1]
+  mae_results_vec[i] <- BS_pi_results[2]
+}
+
+loss_results_vec
+mae_results_vec
